e51746d66a0ec56789183eec3d2aee946e493bb2
NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>
2021-07-14 09:34:54 -0400

    Initial commit
    Signed-off-by: NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>

--- src/main/java/Desastres.java
+++ src/main/java/Desastres.java
@@ -1,2 +1,55 @@
 public class Desastres {
+    private String Hora;
+    private String Daños;
+    private String Dia;
+    private String epicentro;
+    private int cantidad;
+
+    public Desastres(String hora, String daños, String dia, String epicentro, int cantidad) {
+        Hora = hora;
+        Daños = daños;
+        Dia = dia;
+        this.epicentro = epicentro;
+        this.cantidad = cantidad;
+    }
+
+    public String getHora() {
+        return Hora;
+    }
+
+    public void setHora(String hora) {
+        Hora = hora;
+    }
+
+    public String getDaños() {
+        return Daños;
+    }
+
+    public void setDaños(String daños) {
+        Daños = daños;
+    }
+
+    public String getDia() {
+        return Dia;
+    }
+
+    public void setDia(String dia) {
+        Dia = dia;
+    }
+
+    public String getEpicentro() {
+        return epicentro;
+    }
+
+    public void setEpicentro(String epicentro) {
+        this.epicentro = epicentro;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
